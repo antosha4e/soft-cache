a34ae668eae1fc7f8417871b4242e10da7953b49
antosha.4e@gmail.com <antosha.4e@gmail.com@5f2ff701-0caa-dc0e-b521-c472f4b55c1c>
2012-07-20 14:00:28 +0000

    first blood

---
+++ src/com/tosha/Test.java
@@ -0,0 +1,12 @@
+package com.tosha;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: arsentyev
+ * Date: 20.07.12
+ */
+public class Test {
+    public static void main(String... args) {
+        ;
+    }
+}
